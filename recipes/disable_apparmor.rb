212ed2c2e3020105b650d0166df373723875bf3a
Rahat Mahmood <rmahmood@gridcentric.ca>
2012-09-07 13:33:52 -0400

    Restart relevant services are disabling apparmor profiles for them.

--- recipes/disable_apparmor.rb
+++ recipes/disable_apparmor.rb
@@ -9,3 +9,11 @@ execute "disable apparmor" do
   command "find /etc/apparmor.d -name 'usr*libvirt*' | xargs -n1 -I{} mv {} /etc/apparmor.d/disable"
   action :run
 end
+
+service "apparmor" do
+  action :restart
+end
+
+service "libvirt-bin" do
+  action :restart
+end
