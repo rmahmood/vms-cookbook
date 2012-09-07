769cd3a0a08c359c4ac1d030b32013b665d9e94c
Rahat Mahmood <rmahmood@gridcentric.ca>
2012-09-07 12:56:58 -0400

    Adding a temporary recipe to disable apparmor profiles for libvirtd.

---
+++ recipes/disable_apparmor.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: vms
+# Recipe:: disable_apparmor
+#
+# Copyright 2012, Gridcentric Inc.
+#
+
+execute "disable apparmor" do
+  command "find /etc/apparmor.d -name 'usr*libvirt*' | xargs -n1 -I{} mv {} /etc/apparmor.d/disable"
+  action :run
+end
