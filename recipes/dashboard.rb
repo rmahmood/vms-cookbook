529909512409ee0caaa4b5a8a3cabd55293c198a
alop <alopgeek@gmail.com>
2013-03-04 16:55:31 -0800

    There should be a simple recipe for adding the horizon-gridcentric packages

---
+++ recipes/dashboard.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: vms
+# Recipe:: client
+#
+# Copyright 2012, Gridcentric Inc.
+#
+
+include_recipe "apt"
+
+::Chef::Resource::AptRepository.send(:include, Gridcentric::Vms::Helpers)
+
+if not platform?("ubuntu")
+  raise "Unsupported platform: #{node["platform"]}"
+end
+
+apt_repository "gridcentric-#{node["vms"]["os-version"]}" do
+  uri construct_repo_uri(node["vms"]["os-version"], node)
+  components ["gridcentric", "multiverse"]
+  key construct_key_uri(node)
+  notifies :run, resources(:execute => "apt-get update"), :immediately
+  only_if { platform?("ubuntu") }
+end
+
+package "horizon-gridcentric" do
+  action :install
+  options "-o APT::Install-Recommends=0"
+end
