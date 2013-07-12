6ffa98582c801383388a5159ad758fb55fa8c77e
Andres Lagar-Cavilla <andres@lagarcavilla.org>
2013-07-12 13:46:03 -0400

    Bump version to 1.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@gridcentric.com"
 license          "All rights reserved"
 description      "Installs/Configures vms"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 %w{ ubuntu centos }.each do |os|
   supports os
